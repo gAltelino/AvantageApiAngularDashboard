eefa54c0317474086bd7b9e3f5e79138eeccede6
gAltelino <galtelino@gmail.com>
2019-03-08 00:16:21 -0300

    Angular dashboard api

---
+++ Models/Server.cs
@@ -0,0 +1,10 @@
+namespace Avantage.Api.Models
+{
+    public class Server
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsOnline { get; set; }
+    }
+
+}
